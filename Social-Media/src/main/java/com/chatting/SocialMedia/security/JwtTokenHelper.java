0da64cf82fe22a6ad2ac4c0ee7a516c99e2ac9a6
Sohail <sohail1613@gmail.com>
2022-09-01 01:26:31 +0530

    Rectified configuration

--- Social-Media/src/main/java/com/chatting/SocialMedia/security/JwtTokenHelper.java
+++ Social-Media/src/main/java/com/chatting/SocialMedia/security/JwtTokenHelper.java
@@ -4,5 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenHelper {
+    public static final long JWT_TOKEN_VALIDITY = 5*60*60;
+    private String secret = "jwtTokenKey";
 
+    //retrieve userName from jwt token
+    public String getUserNameFromToken(St)
 }
--- jawab-app/src/main/java/com/jawab/blog/config/SecurityConfiguration.java
+++ jawab-app/src/main/java/com/jawab/blog/config/SecurityConfiguration.java
@@ -8,12 +8,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -29,27 +33,24 @@ public class SecurityConfiguration {
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     //https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+    //https://www.codejava.net/frameworks/spring-boot/fix-websecurityconfigureradapter-deprecated
     @Bean
     protected void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .antMatchers("/api/v1/auth/login").permitAll() //to make url public
+                .antMatchers("/api/v1/auth").permitAll() //to make url public
                 .anyRequest()
                 .authenticated()
                 .and()
-                .exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+                .exceptionHandling()
+                .authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         httpSecurity.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
-
-//    @Bean
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-//        auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -61,10 +62,10 @@ public class SecurityConfiguration {
         authenticationManagerBuilder.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
     }
 
+    //https://www.codejava.net/frameworks/spring-boot/fix-websecurityconfigureradapter-deprecated
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
 }
